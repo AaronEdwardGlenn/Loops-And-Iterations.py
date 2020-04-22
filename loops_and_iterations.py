1e87b346ccaa5fc27738edd9a8cfc2c5068ea201
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-22 10:57:08 -0700

    Working through nested for loops

---
+++ loops_and_iterations.py
@@ -0,0 +1,32 @@
+# here is our basic for loop setup
+nums = [1, 2, 3, 4, 5]
+
+for intigers in nums:
+    print(intigers)
+
+# here we are going to add a break with an if statemnt
+numbers = [1, 2, 3, 4, 5, 6]
+
+for intigers in numbers:
+    if intigers == 3:
+        print('Found!')
+        break
+    print(intigers)
+
+# here we will continue with the loop after printing what we are looking for
+numbers = [1, 2, 3, 4, 5, 6]
+
+for intigers in numbers:
+    if intigers == 3:
+        print('Found!')
+        continue
+    print(intigers)
+
+# we can nest for loops to perform actions on each run through the set
+numbers = [1, 2, 3, 4, 5, 6]
+
+for intigers in numbers:
+    for colors in ['blue', 'green', 'red']:
+        print(intigers, colors)
+
+ # range is a good tool for setting the number of times we want to loop through the set
